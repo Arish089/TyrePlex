f990a67a58ba2909f369603da07420c597ded472
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-08-29 01:30:31 +0530

    tyreplex

---
+++ index.js
@@ -0,0 +1,397 @@
+const data = [
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIy-oLEDS7u-e93zPPZxAkYb9I3gY4jSD2mQ&s",
+        "name": "AMAZER 4G LIFE",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "155/65 R13",
+        "rating": 4.3,
+        "reviews": 320,
+        "price": 3401,
+        "tube": "Tubeless"
+    },    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv7Z1oJ48sdYRu4E2oWujdwI784HlgDYC_Qw&s",
+        "name": "NILOGRIP ZAPPER",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "100/70 ",
+        "rating": 4,
+        "reviews": 473,
+        "price": 3150,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv7Z1oJ48sdYRu4E2oWujdwI784HlgDYC_Qw&s",
+        "name": "NILOGRIP ZAPPER",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "100/70 ",
+        "rating": 4,
+        "reviews": 473,
+        "price": 3150,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv7Z1oJ48sdYRu4E2oWujdwI784HlgDYC_Qw&s",
+        "name": "NILOGRIP ZAPPER",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "100/70 ",
+        "rating": 4,
+        "reviews": 473,
+        "price": 3150,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv7Z1oJ48sdYRu4E2oWujdwI784HlgDYC_Qw&s",
+        "name": "NILOGRIP ZAPPER",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "100/70 ",
+        "rating": 4,
+        "reviews": 473,
+        "price": 3150,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv7Z1oJ48sdYRu4E2oWujdwI784HlgDYC_Qw&s",
+        "name": "NILOGRIP ZAPPER",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "100/70 ",
+        "rating": 4,
+        "reviews": 473,
+        "price": 3150,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fUo3J2zuxY5ON7e4XQfBBReMm3z7Udrrkw&s",
+        "name": "MILAZE X3",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "145/75 R13",
+        "rating": 4.2,
+        "reviews": 188,
+        "price": 2990,
+        "tube": "Tube"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fUo3J2zuxY5ON7e4XQfBBReMm3z7Udrrkw&s",
+        "name": "MILAZE X3",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "145/75 R13",
+        "rating": 4.2,
+        "reviews": 188,
+        "price": 2990,
+        "tube": "Tube"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fUo3J2zuxY5ON7e4XQfBBReMm3z7Udrrkw&s",
+        "name": "MILAZE X3",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "145/75 R13",
+        "rating": 4.2,
+        "reviews": 188,
+        "price": 2910,
+        "tube": "Tube"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fUo3J2zuxY5ON7e4XQfBBReMm3z7Udrrkw&s",
+        "name": "MILAZE X3",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "145/75 R13",
+        "rating": 4.2,
+        "reviews": 188,
+        "price": 2890,
+        "tube": "Tubeless"
+    },
+    {
+        "brand": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fUo3J2zuxY5ON7e4XQfBBReMm3z7Udrrkw&s",
+        "name": "MILAZE X3",
+        "image": "https://crdms.images.consumerreports.org/w_263,f_auto,q_auto/prod/products/cr/models/408842-all-season-suv-tires-yokohama-geolandar-cv-g058-10036344",
+        "model": "145/75 R13",
+        "rating": 4.2,
+        "reviews": 188,
+        "price": 2950,
+        "tube": "Tubeless"
+    }
+
+]
+
+let SortChange = false;
+let FilterChange = false;
+
+const itemsPerPage = 10;
+let currentPage = 1;
+
+let rootElement = document.getElementById("items");
+
+let next = document.getElementById('next');
+let previous = document.getElementById('previous');
+
+let PageNo = document.getElementById("currentPage");
+
+let selectedBrand = document.getElementById('filter');
+let sortedBrand = document.getElementById('sort');
+
+// Slider Functionality
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollRight = document.getElementById("scrollRight");
+    const productListContainer = document.querySelector(".slider-container");
+  
+    console.log("Initial Scroll Left:", productListContainer.scrollLeft);
+  
+    scrollRight.addEventListener("click", function() {
+      console.log("Clicked!");
+      productListContainer.scrollBy({
+        top: 0, 
+        left: 620, 
+        behavior: 'smooth'
+      });
+      console.log("Scroll Left After:", productListContainer.scrollLeft);
+    });
+  });
+
+  //dynamic loading of tyres
+  const displayTires = (data,parentNode)=>{
+    
+    rootElement.innerHTML = '';
+
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = currentPage * itemsPerPage
+    data.slice(start, end).forEach(ele => {
+        let card = document.createElement('div');
+        card.className = "card mx-auto rounded py-4 shadow border border-light"
+        card.style.width = '18em';
+        card.style.textAlign = 'center'
+
+        let cardImage = document.createElement('img');
+        cardImage.src = ele.image
+        cardImage.style.width = '80%'
+        cardImage.style.height = '120px'
+        cardImage.style.margin = 'auto'
+
+        let ChildCard = document.createElement('div');
+        ChildCard.className = 'card-body';
+
+        let logo = document.createElement('img');
+        logo.src = ele.brand;
+        logo.style.width = '70%'
+        
+        let modelName = document.createElement('p');
+        modelName.className = 'card-text';
+        modelName.textContent = ele.name
+        modelName.style.fontWeight = '500'
+
+        ChildCard.append(logo, modelName);
+
+        let list = document.createElement('ul');
+        list.className = 'list-group list-group-flush';
+
+        let listItem1 = document.createElement('li');
+        listItem1.className = 'list-group-item';
+        listItem1.style.fontWeight = '600'
+        listItem1.textContent = `${ele.model} ${ele.tube}`
+        listItem1.style.color = 'grey'
+
+        let innerList = document.createElement('div')
+        innerList.className = 'd-flex justify-content-center border border-0 py-2';
+        innerList.style.border = 'none'
+
+        let listItem2 = document.createElement('i');
+        listItem2.className = 'list-group-item bi bi-star-fill rounded';
+        listItem2.style.fontStyle = 'normal'
+        listItem2.style.backgroundColor = 'lightGreen'
+        listItem2.style.color = 'white' 
+        listItem2.textContent = ele.rating;
+
+        let listItem3 = document.createElement('li');
+        listItem3.className = 'list-group-item mx-2';
+        listItem3.textContent = `${ele.reviews} Reviews`
+        listItem3.style.backgroundColor = 'skyBlue'
+        listItem3.style.color = 'white'
+
+        innerList.append(listItem2, listItem3)
+
+        let innerList2 = document.createElement('div');
+        innerList2.className = 'd-flex align-items-center mx-auto rounded'
+
+        let warranty = document.createElement('li')
+        warranty.innerHTML = '<i class="bi bi-shield-fill-check" style="color:green; font-style:normal;">&nbsp; 5 year warranty &nbsp;<i/>';
+        warranty.style.backgroundColor = 'gold';
+        warranty.style.margin = '4px';
+        warranty.style.color = 'white';
+        warranty.style.fontWeight = '500';
+        warranty.style.fontStyle = 'normal'
+
+        let listItem4 = document.createElement('i');
+        listItem4.className = 'list-group-item bi bi-currency-rupee';
+        listItem4.textContent = ele.price;
+        listItem4.style.fontStyle = 'normal';
+        listItem4.style.fontWeight = '500'
+        listItem4.style.backgroundColor = 'darkcyan'
+        listItem4.style.color = 'white'
+
+        innerList2.append(listItem4, warranty)
+
+        list.append(listItem1, innerList, innerList2)
+
+        card.append(cardImage, ChildCard, list)
+
+    parentNode.append(card)
+    })
+    console.log(currentPage);
+   PageNo.textContent = currentPage
+   PageNo.style.fontSize = '20px'
+  }
+
+  //previous page
+  next.addEventListener('click', () => {
+   currentPage < Math.ceil(data.length/itemsPerPage) ? currentPage++ : null
+    displayTires(data, rootElement);
+    
+});
+// next page
+previous.addEventListener('click', () => {
+    currentPage > 1 ? currentPage-- : null
+     displayTires(data, rootElement);
+     
+ });
+
+ // logic for sorting
+function sortLogic (param, data,parentNode){ 
+    if (param === "popular") {
+        data.sort((a, b) => b.reviews - a.reviews); // Descending by reviews (popular)
+    } else if (param === "desc") {
+        data.sort((a, b) => b.price - a.price); // Descending by price
+    } else {
+        data.sort((a, b) => a.price - b.price); // Ascending by price (default)
+    }
+
+    displayTires(data, parentNode)
+    return data
+}
+
+// logic for filtering (Sorting is called here only)
+function FilterAndSort(param,param1,data,parentNode){
+    let temp = data
+
+    if(param === "All"){
+        sortLogic(param1,temp,parentNode)
+        displayTires(temp,parentNode)
+        return temp
+    }else{
+        filteredItems = data.filter((ele)=>ele.name === param);
+        sortLogic(param1,filteredItems,parentNode )
+        displayTires(filteredItems, parentNode)
+        return filteredItems
+    }
+}
+
+document.getElementById('filter').addEventListener('change',()=>{
+    currentPage = 1
+    FilterAndSort(selectedBrand.value, sortedBrand.value,data,rootElement)
+})
+
+document.getElementById('sort').addEventListener('change',()=>{
+    currentPage = 1
+    FilterAndSort(selectedBrand.value, sortedBrand.value,data,rootElement)
+})
+
+window.onload = function() {
+    // Code to be executed when the page is fully loaded
+displayTires(data, rootElement)  
+};
